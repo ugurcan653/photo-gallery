3091e8a0a663ead6011b479cd93cf3f2f1ca349d
ucakar <ucakar@lely.com>
2022-01-26 18:27:47 +0300

    ilk-kursun

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'nl.lely.mobile.assistantNext',
   appName: 'photo-gallery',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
 import { Component } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Network } from '@capacitor/network';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { Browser } from '@capacitor/browser';
+import {ActionPerformed,PushNotificationSchema,PushNotifications,Token} from '@capacitor/push-notifications';
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  photo: any;
+  status: any;
+  title = 'capacitor-app';
+  barcodeData: any;
+  token: string = "karpuz";
+  constructor(private sn: DomSanitizer, private barcodeScanner: BarcodeScanner) {
+  }
+  scanner() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.barcodeData = barcodeData;
+     }).catch(err => {
+      this.barcodeData =  err;
+     });
+  }
+
+  async takePicture(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+    var imageUrl = image.webPath as string;
+    this.photo = this.sn.bypassSecurityTrustResourceUrl(imageUrl)
+  };
+  async logCurrentNetworkStatus(){
+    this.status = await Network.getStatus();
+  };
+  async openCapacitorSite(){
+    await Browser.open({ url: 'http://capacitorjs.com/', });
+  };
+  initFCM(){
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        PushNotifications.register();
+      } else {
+      }
+    });
+
+    PushNotifications.addListener('registration', (token: Token) => {
+       this.token = token.value;
+    });
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      },
+    );
+
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      },
+    );
+  }
 }
